d034e14e87afeb244012f5774b09ca84b3a4d527
Jeremy Penaredondo <jay.pena1224@proton.me>
2023-10-13 15:18:52 -0400

    adds readStatus property to book object

--- library.js
+++ library.js
@@ -4,10 +4,19 @@ const authorInput = document.querySelector('#authorInput');
 const addButton = document.querySelector('#addButton');
 const bookList = document.querySelector('#bookList');
 
-function Book(title, author) {
+function Book(title, author, status) {
     this.id = library.length;
     this.title = title;
     this.author = author;
+    this.readStatus = status;
+}
+
+Book.prototype.setStatus = () => {
+    if (this.readStatus == 'Read') {
+        this.readStatus = 'Unread';
+    } else {
+        this.readStatus = 'Read';
+    }
 }
 
 function addBookToLibrary() {
@@ -15,7 +24,6 @@ function addBookToLibrary() {
     let author = authorInput.value;
     let newBook = new Book(title, author);
     library.push(newBook);
-    populateList(library);
 }
 
 function removeBook(id) {
@@ -23,7 +31,6 @@ function removeBook(id) {
         return book.id == id;
     });
     library.splice(target, 1);
-    populateList(library);
 }
 
 function createDeletebutton(id) {
@@ -32,8 +39,24 @@ function createDeletebutton(id) {
     deleteButton.setAttribute('id', id);
     deleteButton.addEventListener('click', e => {
         removeBook(e.target.id);
+        populateList(library);
     });
     return deleteButton;
+}
+
+function createStatusButton(book) {
+    let statusButton = document.createElement('button');
+    statusButton.innerText = book.readStatus;
+    statusButton.setAttribute('id', book.id);
+    statusButton.addEventListener('click', e => {
+        library[e.target.id].setStatus();
+        if (statusButton.innerText === 'Read') {
+            statusButton.innerText = 'Unread';
+        } else {
+            statusButton.innerText = 'Read';
+        }
+    })
+    return statusButton;
 }
 
 function createCard(book) {
@@ -41,15 +64,18 @@ function createCard(book) {
     let title = document.createElement('p');
     let author = document.createElement('p');
     let deleteButton = createDeletebutton(book.id);
+    let statusButton = createStatusButton(book);
     title.innerText = book.title;
     author.innerText = book.author;
     div.appendChild(title);
     div.appendChild(author);
+    div.appendChild(statusButton);
     div.appendChild(deleteButton);
+    div.setAttribute('class', 'card');
     bookList.appendChild(div);  
 }
 
-let bookOne = new Book("Book One", "Author");
+let bookOne = new Book("Book One", "Author", "Read");
 library.push(bookOne);
 
 function populateList(arr) {
@@ -63,4 +89,7 @@ function populateList(arr) {
 
 populateList(library);
 
-addButton.addEventListener('click', addBookToLibrary);
+addButton.addEventListener('click', () => {
+    addBookToLibrary();
+    populateList(library);
+});
